fcd9f3383720dff1411afa914ea63f77ebfd40fb
Jessica Ji <jji@berkeley.edu>
2016-01-12 13:56:57 -0800

    Contact form edits

--- js/classie.js
+++ js/classie.js
@@ -1,6 +1,5 @@
 /*!
  * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
  * 
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
@@ -14,14 +13,10 @@
 
 'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
-
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 }
 
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
@@ -55,12 +50,10 @@ function toggleClass( elem, c ) {
 }
 
 window.classie = {
-  // full names
   hasClass: hasClass,
   addClass: addClass,
   removeClass: removeClass,
   toggleClass: toggleClass,
-  // short names
   has: hasClass,
   add: addClass,
   remove: removeClass,
