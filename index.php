7f6e524a14ad60e995b4d64f7069b0723d3272b2
Jessica Ji <jji@berkeley.edu>
2016-01-14 20:52:19 -0800

    portfolio edits

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: TangledDemo'; 
+    $from = 'From: $name'; 
     $to = 'jji@berkeley.edu'; 
     $subject = 'A message from $name';
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
-    }
+	    if (mail ($to, $subject, $body, $from)) { 
+	        echo '<p>Your message has been sent!</p>';
+	    } else { 
+	        echo '<p>Something went wrong, go back and try again!</p>'; 
+	    }
 	}
 ?>
